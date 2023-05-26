d94824206860c66dfd3e00d0fc89b6dccffe3645
lan2405 <2405349439@qq.com>
2023-05-26 20:46:31 +0800

    添加项目文件。

---
+++ dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h: 模块类的声明。
+
+class CmainModule : public ATL::CAtlDllModuleT< CmainModule >
+{
+public :
+	DECLARE_LIBID(LIBID_mainLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_MAIN, "{f17db82d-29b8-4245-9ea9-6182eea7bd33}")
+};
+
+extern class CmainModule _AtlModule;
