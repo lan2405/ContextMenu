9b270c4e0ac6828e6211a91f08b62ea985743d49
lan2405 <2405349439@qq.com>
2023-05-26 21:05:52 +0800

    添加类

---
+++ ContextMenuGreat.cpp
@@ -0,0 +1,8 @@
+﻿// ContextMenuGreat.cpp: CContextMenuGreat 的实现
+
+#include "pch.h"
+#include "ContextMenuGreat.h"
+
+
+// CContextMenuGreat
+
--- main_i.h
+++ main_i.h
@@ -1,1 +1,478 @@
-﻿/* MIDL:  总是生成此文件，它包含接口的定义 */
+
+
+/* this ALWAYS GENERATED file contains the definitions for the interfaces */
+
+
+ /* File created by MIDL compiler version 8.01.0628 */
+/* at Tue Jan 19 11:14:07 2038
+ */
+/* Compiler settings for main.idl:
+    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.01.0628 
+    protocol : all , ms_ext, c_ext, robust
+    error checks: allocation ref bounds_check enum stub_data 
+    VC __declspec() decoration level: 
+         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+         DECLSPEC_UUID(), MIDL_INTERFACE()
+*/
+/* @@MIDL_FILE_HEADING(  ) */
+
+
+
+/* verify that the <rpcndr.h> version is high enough to compile this file*/
+#ifndef __REQUIRED_RPCNDR_H_VERSION__
+#define __REQUIRED_RPCNDR_H_VERSION__ 500
+#endif
+
+#include "rpc.h"
+#include "rpcndr.h"
+
+#ifndef __RPCNDR_H_VERSION__
+#error this stub requires an updated version of <rpcndr.h>
+#endif /* __RPCNDR_H_VERSION__ */
+
+#ifndef COM_NO_WINDOWS_H
+#include "windows.h"
+#include "ole2.h"
+#endif /*COM_NO_WINDOWS_H*/
+
+#ifndef __main_i_h__
+#define __main_i_h__
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#ifndef DECLSPEC_XFGVIRT
+#if defined(_CONTROL_FLOW_GUARD_XFG)
+#define DECLSPEC_XFGVIRT(base, func) __declspec(xfg_virtual(base, func))
+#else
+#define DECLSPEC_XFGVIRT(base, func)
+#endif
+#endif
+
+/* Forward Declarations */ 
+
+#ifndef __IComponentRegistrar_FWD_DEFINED__
+#define __IComponentRegistrar_FWD_DEFINED__
+typedef interface IComponentRegistrar IComponentRegistrar;
+
+#endif 	/* __IComponentRegistrar_FWD_DEFINED__ */
+
+
+#ifndef __IContextMenuGreat_FWD_DEFINED__
+#define __IContextMenuGreat_FWD_DEFINED__
+typedef interface IContextMenuGreat IContextMenuGreat;
+
+#endif 	/* __IContextMenuGreat_FWD_DEFINED__ */
+
+
+#ifndef __CompReg_FWD_DEFINED__
+#define __CompReg_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class CompReg CompReg;
+#else
+typedef struct CompReg CompReg;
+#endif /* __cplusplus */
+
+#endif 	/* __CompReg_FWD_DEFINED__ */
+
+
+#ifndef __ContextMenuGreat_FWD_DEFINED__
+#define __ContextMenuGreat_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class ContextMenuGreat ContextMenuGreat;
+#else
+typedef struct ContextMenuGreat ContextMenuGreat;
+#endif /* __cplusplus */
+
+#endif 	/* __ContextMenuGreat_FWD_DEFINED__ */
+
+
+/* header files for imported files */
+#include "oaidl.h"
+#include "ocidl.h"
+#include "shobjidl.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+#ifndef __IComponentRegistrar_INTERFACE_DEFINED__
+#define __IComponentRegistrar_INTERFACE_DEFINED__
+
+/* interface IComponentRegistrar */
+/* [unique][dual][uuid][object] */ 
+
+
+EXTERN_C const IID IID_IComponentRegistrar;
+
+#if defined(__cplusplus) && !defined(CINTERFACE)
+    
+    MIDL_INTERFACE("a817e7a2-43fa-11d0-9e44-00aa00b6770a")
+    IComponentRegistrar : public IDispatch
+    {
+    public:
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE Attach( 
+            /* [in] */ BSTR bstrPath) = 0;
+        
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE RegisterAll( void) = 0;
+        
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE UnregisterAll( void) = 0;
+        
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE GetComponents( 
+            /* [out] */ SAFEARRAY * *pbstrCLSIDs,
+            /* [out] */ SAFEARRAY * *pbstrDescriptions) = 0;
+        
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE RegisterComponent( 
+            /* [in] */ BSTR bstrCLSID) = 0;
+        
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE UnregisterComponent( 
+            /* [in] */ BSTR bstrCLSID) = 0;
+        
+    };
+    
+    
+#else 	/* C style interface */
+
+    typedef struct IComponentRegistrarVtbl
+    {
+        BEGIN_INTERFACE
+        
+        DECLSPEC_XFGVIRT(IUnknown, QueryInterface)
+        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
+            IComponentRegistrar * This,
+            /* [in] */ REFIID riid,
+            /* [annotation][iid_is][out] */ 
+            _COM_Outptr_  void **ppvObject);
+        
+        DECLSPEC_XFGVIRT(IUnknown, AddRef)
+        ULONG ( STDMETHODCALLTYPE *AddRef )( 
+            IComponentRegistrar * This);
+        
+        DECLSPEC_XFGVIRT(IUnknown, Release)
+        ULONG ( STDMETHODCALLTYPE *Release )( 
+            IComponentRegistrar * This);
+        
+        DECLSPEC_XFGVIRT(IDispatch, GetTypeInfoCount)
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfoCount )( 
+            IComponentRegistrar * This,
+            /* [out] */ UINT *pctinfo);
+        
+        DECLSPEC_XFGVIRT(IDispatch, GetTypeInfo)
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfo )( 
+            IComponentRegistrar * This,
+            /* [in] */ UINT iTInfo,
+            /* [in] */ LCID lcid,
+            /* [out] */ ITypeInfo **ppTInfo);
+        
+        DECLSPEC_XFGVIRT(IDispatch, GetIDsOfNames)
+        HRESULT ( STDMETHODCALLTYPE *GetIDsOfNames )( 
+            IComponentRegistrar * This,
+            /* [in] */ REFIID riid,
+            /* [size_is][in] */ LPOLESTR *rgszNames,
+            /* [range][in] */ UINT cNames,
+            /* [in] */ LCID lcid,
+            /* [size_is][out] */ DISPID *rgDispId);
+        
+        DECLSPEC_XFGVIRT(IDispatch, Invoke)
+        /* [local] */ HRESULT ( STDMETHODCALLTYPE *Invoke )( 
+            IComponentRegistrar * This,
+            /* [annotation][in] */ 
+            _In_  DISPID dispIdMember,
+            /* [annotation][in] */ 
+            _In_  REFIID riid,
+            /* [annotation][in] */ 
+            _In_  LCID lcid,
+            /* [annotation][in] */ 
+            _In_  WORD wFlags,
+            /* [annotation][out][in] */ 
+            _In_  DISPPARAMS *pDispParams,
+            /* [annotation][out] */ 
+            _Out_opt_  VARIANT *pVarResult,
+            /* [annotation][out] */ 
+            _Out_opt_  EXCEPINFO *pExcepInfo,
+            /* [annotation][out] */ 
+            _Out_opt_  UINT *puArgErr);
+        
+        DECLSPEC_XFGVIRT(IComponentRegistrar, Attach)
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *Attach )( 
+            IComponentRegistrar * This,
+            /* [in] */ BSTR bstrPath);
+        
+        DECLSPEC_XFGVIRT(IComponentRegistrar, RegisterAll)
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *RegisterAll )( 
+            IComponentRegistrar * This);
+        
+        DECLSPEC_XFGVIRT(IComponentRegistrar, UnregisterAll)
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *UnregisterAll )( 
+            IComponentRegistrar * This);
+        
+        DECLSPEC_XFGVIRT(IComponentRegistrar, GetComponents)
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *GetComponents )( 
+            IComponentRegistrar * This,
+            /* [out] */ SAFEARRAY * *pbstrCLSIDs,
+            /* [out] */ SAFEARRAY * *pbstrDescriptions);
+        
+        DECLSPEC_XFGVIRT(IComponentRegistrar, RegisterComponent)
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *RegisterComponent )( 
+            IComponentRegistrar * This,
+            /* [in] */ BSTR bstrCLSID);
+        
+        DECLSPEC_XFGVIRT(IComponentRegistrar, UnregisterComponent)
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE *UnregisterComponent )( 
+            IComponentRegistrar * This,
+            /* [in] */ BSTR bstrCLSID);
+        
+        END_INTERFACE
+    } IComponentRegistrarVtbl;
+
+    interface IComponentRegistrar
+    {
+        CONST_VTBL struct IComponentRegistrarVtbl *lpVtbl;
+    };
+
+    
+
+#ifdef COBJMACROS
+
+
+#define IComponentRegistrar_QueryInterface(This,riid,ppvObject)	\
+    ( (This)->lpVtbl -> QueryInterface(This,riid,ppvObject) ) 
+
+#define IComponentRegistrar_AddRef(This)	\
+    ( (This)->lpVtbl -> AddRef(This) ) 
+
+#define IComponentRegistrar_Release(This)	\
+    ( (This)->lpVtbl -> Release(This) ) 
+
+
+#define IComponentRegistrar_GetTypeInfoCount(This,pctinfo)	\
+    ( (This)->lpVtbl -> GetTypeInfoCount(This,pctinfo) ) 
+
+#define IComponentRegistrar_GetTypeInfo(This,iTInfo,lcid,ppTInfo)	\
+    ( (This)->lpVtbl -> GetTypeInfo(This,iTInfo,lcid,ppTInfo) ) 
+
+#define IComponentRegistrar_GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId)	\
+    ( (This)->lpVtbl -> GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId) ) 
+
+#define IComponentRegistrar_Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr)	\
+    ( (This)->lpVtbl -> Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr) ) 
+
+
+#define IComponentRegistrar_Attach(This,bstrPath)	\
+    ( (This)->lpVtbl -> Attach(This,bstrPath) ) 
+
+#define IComponentRegistrar_RegisterAll(This)	\
+    ( (This)->lpVtbl -> RegisterAll(This) ) 
+
+#define IComponentRegistrar_UnregisterAll(This)	\
+    ( (This)->lpVtbl -> UnregisterAll(This) ) 
+
+#define IComponentRegistrar_GetComponents(This,pbstrCLSIDs,pbstrDescriptions)	\
+    ( (This)->lpVtbl -> GetComponents(This,pbstrCLSIDs,pbstrDescriptions) ) 
+
+#define IComponentRegistrar_RegisterComponent(This,bstrCLSID)	\
+    ( (This)->lpVtbl -> RegisterComponent(This,bstrCLSID) ) 
+
+#define IComponentRegistrar_UnregisterComponent(This,bstrCLSID)	\
+    ( (This)->lpVtbl -> UnregisterComponent(This,bstrCLSID) ) 
+
+#endif /* COBJMACROS */
+
+
+#endif 	/* C style interface */
+
+
+
+
+#endif 	/* __IComponentRegistrar_INTERFACE_DEFINED__ */
+
+
+#ifndef __IContextMenuGreat_INTERFACE_DEFINED__
+#define __IContextMenuGreat_INTERFACE_DEFINED__
+
+/* interface IContextMenuGreat */
+/* [unique][nonextensible][dual][uuid][object] */ 
+
+
+EXTERN_C const IID IID_IContextMenuGreat;
+
+#if defined(__cplusplus) && !defined(CINTERFACE)
+    
+    MIDL_INTERFACE("bb0d4f03-31d1-4b59-8296-7bac734f2d67")
+    IContextMenuGreat : public IDispatch
+    {
+    public:
+    };
+    
+    
+#else 	/* C style interface */
+
+    typedef struct IContextMenuGreatVtbl
+    {
+        BEGIN_INTERFACE
+        
+        DECLSPEC_XFGVIRT(IUnknown, QueryInterface)
+        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
+            IContextMenuGreat * This,
+            /* [in] */ REFIID riid,
+            /* [annotation][iid_is][out] */ 
+            _COM_Outptr_  void **ppvObject);
+        
+        DECLSPEC_XFGVIRT(IUnknown, AddRef)
+        ULONG ( STDMETHODCALLTYPE *AddRef )( 
+            IContextMenuGreat * This);
+        
+        DECLSPEC_XFGVIRT(IUnknown, Release)
+        ULONG ( STDMETHODCALLTYPE *Release )( 
+            IContextMenuGreat * This);
+        
+        DECLSPEC_XFGVIRT(IDispatch, GetTypeInfoCount)
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfoCount )( 
+            IContextMenuGreat * This,
+            /* [out] */ UINT *pctinfo);
+        
+        DECLSPEC_XFGVIRT(IDispatch, GetTypeInfo)
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfo )( 
+            IContextMenuGreat * This,
+            /* [in] */ UINT iTInfo,
+            /* [in] */ LCID lcid,
+            /* [out] */ ITypeInfo **ppTInfo);
+        
+        DECLSPEC_XFGVIRT(IDispatch, GetIDsOfNames)
+        HRESULT ( STDMETHODCALLTYPE *GetIDsOfNames )( 
+            IContextMenuGreat * This,
+            /* [in] */ REFIID riid,
+            /* [size_is][in] */ LPOLESTR *rgszNames,
+            /* [range][in] */ UINT cNames,
+            /* [in] */ LCID lcid,
+            /* [size_is][out] */ DISPID *rgDispId);
+        
+        DECLSPEC_XFGVIRT(IDispatch, Invoke)
+        /* [local] */ HRESULT ( STDMETHODCALLTYPE *Invoke )( 
+            IContextMenuGreat * This,
+            /* [annotation][in] */ 
+            _In_  DISPID dispIdMember,
+            /* [annotation][in] */ 
+            _In_  REFIID riid,
+            /* [annotation][in] */ 
+            _In_  LCID lcid,
+            /* [annotation][in] */ 
+            _In_  WORD wFlags,
+            /* [annotation][out][in] */ 
+            _In_  DISPPARAMS *pDispParams,
+            /* [annotation][out] */ 
+            _Out_opt_  VARIANT *pVarResult,
+            /* [annotation][out] */ 
+            _Out_opt_  EXCEPINFO *pExcepInfo,
+            /* [annotation][out] */ 
+            _Out_opt_  UINT *puArgErr);
+        
+        END_INTERFACE
+    } IContextMenuGreatVtbl;
+
+    interface IContextMenuGreat
+    {
+        CONST_VTBL struct IContextMenuGreatVtbl *lpVtbl;
+    };
+
+    
+
+#ifdef COBJMACROS
+
+
+#define IContextMenuGreat_QueryInterface(This,riid,ppvObject)	\
+    ( (This)->lpVtbl -> QueryInterface(This,riid,ppvObject) ) 
+
+#define IContextMenuGreat_AddRef(This)	\
+    ( (This)->lpVtbl -> AddRef(This) ) 
+
+#define IContextMenuGreat_Release(This)	\
+    ( (This)->lpVtbl -> Release(This) ) 
+
+
+#define IContextMenuGreat_GetTypeInfoCount(This,pctinfo)	\
+    ( (This)->lpVtbl -> GetTypeInfoCount(This,pctinfo) ) 
+
+#define IContextMenuGreat_GetTypeInfo(This,iTInfo,lcid,ppTInfo)	\
+    ( (This)->lpVtbl -> GetTypeInfo(This,iTInfo,lcid,ppTInfo) ) 
+
+#define IContextMenuGreat_GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId)	\
+    ( (This)->lpVtbl -> GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId) ) 
+
+#define IContextMenuGreat_Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr)	\
+    ( (This)->lpVtbl -> Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr) ) 
+
+
+#endif /* COBJMACROS */
+
+
+#endif 	/* C style interface */
+
+
+
+
+#endif 	/* __IContextMenuGreat_INTERFACE_DEFINED__ */
+
+
+
+#ifndef __mainLib_LIBRARY_DEFINED__
+#define __mainLib_LIBRARY_DEFINED__
+
+/* library mainLib */
+/* [custom][version][uuid] */ 
+
+
+EXTERN_C const IID LIBID_mainLib;
+
+EXTERN_C const CLSID CLSID_CompReg;
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("7e9a7989-5402-4cff-a8b8-cf769972d901")
+CompReg;
+#endif
+
+EXTERN_C const CLSID CLSID_ContextMenuGreat;
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("f769a6c4-e8c1-4343-8363-5a751b1f27d7")
+ContextMenuGreat;
+#endif
+#endif /* __mainLib_LIBRARY_DEFINED__ */
+
+/* Additional Prototypes for ALL interfaces */
+
+unsigned long             __RPC_USER  BSTR_UserSize(     unsigned long *, unsigned long            , BSTR * ); 
+unsigned char * __RPC_USER  BSTR_UserMarshal(  unsigned long *, unsigned char *, BSTR * ); 
+unsigned char * __RPC_USER  BSTR_UserUnmarshal(unsigned long *, unsigned char *, BSTR * ); 
+void                      __RPC_USER  BSTR_UserFree(     unsigned long *, BSTR * ); 
+
+unsigned long             __RPC_USER  LPSAFEARRAY_UserSize(     unsigned long *, unsigned long            , LPSAFEARRAY * ); 
+unsigned char * __RPC_USER  LPSAFEARRAY_UserMarshal(  unsigned long *, unsigned char *, LPSAFEARRAY * ); 
+unsigned char * __RPC_USER  LPSAFEARRAY_UserUnmarshal(unsigned long *, unsigned char *, LPSAFEARRAY * ); 
+void                      __RPC_USER  LPSAFEARRAY_UserFree(     unsigned long *, LPSAFEARRAY * ); 
+
+unsigned long             __RPC_USER  BSTR_UserSize64(     unsigned long *, unsigned long            , BSTR * ); 
+unsigned char * __RPC_USER  BSTR_UserMarshal64(  unsigned long *, unsigned char *, BSTR * ); 
+unsigned char * __RPC_USER  BSTR_UserUnmarshal64(unsigned long *, unsigned char *, BSTR * ); 
+void                      __RPC_USER  BSTR_UserFree64(     unsigned long *, BSTR * ); 
+
+unsigned long             __RPC_USER  LPSAFEARRAY_UserSize64(     unsigned long *, unsigned long            , LPSAFEARRAY * ); 
+unsigned char * __RPC_USER  LPSAFEARRAY_UserMarshal64(  unsigned long *, unsigned char *, LPSAFEARRAY * ); 
+unsigned char * __RPC_USER  LPSAFEARRAY_UserUnmarshal64(unsigned long *, unsigned char *, LPSAFEARRAY * ); 
+void                      __RPC_USER  LPSAFEARRAY_UserFree64(     unsigned long *, LPSAFEARRAY * ); 
+
+/* end of Additional Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
